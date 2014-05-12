38dbd197a9ff4d400fc13cc49e13da4058bf182e
Jason Long <jason@long.name>
2014-05-12 14:14:41 -0400

    show initial infections and player's hands

--- webapp/pandemic-duplicate.js
+++ webapp/pandemic-duplicate.js
@@ -84,7 +84,8 @@ function generate_decks()
 	for (var i = 0; i < G.rules.player_count; i++) {
 		var b = [];
 		for (var j = 0; j < hand_size; j++) {
-			b.push[A.pop()];
+			var c = A.pop();
+			b.push(c);
 		}
 		G.initial_hands[(1+i)] = b;
 	}
@@ -128,16 +129,32 @@ function submit_create_game_form()
 	generate_decks();
 
 	show_page("player_names_page");
+	var $pg = $('#player_names_page');
+	if (G.rules.player_count <= 2) {
+		$('.player3', $pg).hide();
+		$('.player4', $pg).hide();
+	}
+	else if (G.rules.player_count == 3) {
+		$('.player3', $pg).show();
+		$('.player4', $pg).hide();
+	}
+	else {
+		$('.player3', $pg).show();
+		$('.player4', $pg).show();
+	}
+
 	return false;
 }
 
 function submit_player_names_form()
 {
 	var f = document.player_names_form;
-	var p1 = f.player1.value;
-	var p2 = f.player2.value;
-	var p3 = f.player3.value;
-	var p4 = f.player4.value;
+	G.player_names = {
+		'1': f.player1.value,
+		'2': f.player2.value,
+		'3': f.player3.value,
+		'4': f.player4.value
+		};
 	var randomize = f.randomize_order.checked;
 
 	show_page('deck_setup_page');
@@ -158,6 +175,71 @@ function submit_player_names_form()
 	}
 
 	return false;
+}
+
+function continue_after_deck_setup()
+{
+	show_page('board_setup_page');
+
+	$('.3cube_cities').empty();
+	for (var i = 0; i < 3; i++) {
+		var c = G.infection_deck[i];
+		var $x = $('<li></li>');
+		$x.text(c);
+		$('.3cube_cities').append($x);
+	}
+
+	$('.2cube_cities').empty();
+	for (var i = 3; i < 6; i++) {
+		var c = G.infection_deck[i];
+		var $x = $('<li></li>');
+		$x.text(c);
+		$('.2cube_cities').append($x);
+	}
+
+	$('.1cube_cities').empty();
+	for (var i = 6; i < 9; i++) {
+		var c = G.infection_deck[i];
+		var $x = $('<li></li>');
+		$x.text(c);
+		$('.1cube_cities').append($x);
+	}
+}
+
+function continue_after_board_setup()
+{
+	show_page('player_setup_page');
+
+	var $pg = $('#player_setup_page');
+	if (G.rules.player_count <= 2) {
+		$('.player3', $pg).hide();
+		$('.player4', $pg).hide();
+	}
+	else if (G.rules.player_count == 3) {
+		$('.player3', $pg).show();
+		$('.player4', $pg).hide();
+	}
+	else {
+		$('.player3', $pg).show();
+		$('.player4', $pg).show();
+	}
+
+	for (var i = 1; i <= G.rules.player_count; i++) {
+		$('.player'+i+' .player_name', $pg).text(G.player_names[i]);
+		$('.player'+i+' .card_list', $pg).empty();
+		for (var j = 0; j < G.initial_hands[i].length; j++) {
+			var c = G.initial_hands[i][j];
+			var $c = make_player_card(c);
+			$('.player'+i+' .card_list', $pg).append($c);
+		}
+	}
+}
+
+function make_player_card(c)
+{
+	var $x = $('<li></li>');
+	$x.text(c);
+	return $x;
 }
 
 function show_page(page_name)
