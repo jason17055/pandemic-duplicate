09853047c49b79101043ace898f298348f95ea17
Jason Long <jason@long.name>
2016-04-04 19:59:40 -0700

    angularize: define Controller for each page

--- webapp/app.js
+++ webapp/app.js
@@ -28,3 +28,107 @@ app.controller('TopController',
       on_state_init();
     });
   });
+
+app.controller('WelcomePageController',
+  function() {
+  });
+
+app.controller('OptionsPageController',
+  function() {
+  });
+
+app.controller('SubscriptionPageController',
+  function() {
+  });
+
+app.controller('CreateGamePageController',
+  function() {
+  });
+
+app.controller('GenerateGamePageController',
+  function() {
+  });
+
+app.controller('JoinGamePageController',
+  function() {
+  });
+
+app.controller('JoinGamePickPageController',
+  function() {
+  });
+
+app.controller('PickScenarioPageController',
+  function() {
+  });
+
+app.controller('ReviewResultsPageController',
+  function() {
+  });
+
+app.controller('FoundCompletedGamesPageController',
+  function() {
+  });
+
+app.controller('PlayerNamesPageController',
+  function() {
+  });
+
+app.controller('DeckSetupPageController',
+  function() {
+  });
+
+app.controller('BoardSetupPageController',
+  function() {
+  });
+
+app.controller('PlayerSetupPageController',
+  function() {
+  });
+
+app.controller('PlayerTurnPageController',
+  function() {
+  });
+
+app.controller('DiscoverCurePageController',
+  function() {
+  });
+
+app.controller('VirulentStrainPageController',
+  function() {
+  });
+
+app.controller('SpecialEventPageController',
+  function() {
+  });
+
+app.controller('NewAssignmentPageController',
+  function() {
+  });
+
+app.controller('ResilientPopulationPageController',
+  function() {
+  });
+
+app.controller('InfectionRumorPageController',
+  function() {
+  });
+
+app.controller('ForecastPageController',
+  function() {
+  });
+
+app.controller('ResourcePlanningPageController',
+  function() {
+  });
+
+app.controller('GameCompletedPageController',
+  function() {
+  });
+
+app.controller('ResultsPageController',
+  function() {
+  });
+
+app.controller('ShowDiscardsPageController',
+  function() {
+  });
