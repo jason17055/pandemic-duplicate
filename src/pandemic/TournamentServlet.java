fa8ea68b2c386b98774db338db1962087516f20d
Jason Long <jason@long.name>
2016-04-19 20:13:07 -0700

    tournaments: implement GET for tournament servlet

--- src/pandemic/TournamentServlet.java
+++ src/pandemic/TournamentServlet.java
@@ -1,17 +1,11 @@
 package pandemic;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
+import java.security.Principal;
 import java.util.logging.Logger;
 import javax.servlet.http.*;
 import com.fasterxml.jackson.core.*;
 import com.google.appengine.api.datastore.*;
-import com.google.appengine.api.users.*;
-
-import static pandemic.HelperFunctions.*;
-import static pandemic.PandemicDealServlet.getRequestContent;
-import static pandemic.PandemicDealServlet.MAX_PLAYERS;
 
 public class TournamentServlet extends HttpServlet
 {
@@ -21,7 +15,55 @@ public class TournamentServlet extends HttpServlet
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException
 	{
-		doGetIndex(req, resp);
+		if (req.getParameter("id") != null) {
+			doGetEntity(req.getParameter("id"), req, resp);
+		}
+		else {
+			doGetIndex(req, resp);
+		}
+	}
+
+	boolean isUser(HttpServletRequest req, Key userKey)
+	{
+		if (userKey == null) {
+			return false;
+		}
+
+		Principal p = req.getUserPrincipal();
+		if (p == null) {
+			return false;
+		}
+
+		String username = p.getName();
+		Key refKey = KeyFactory.createKey("User", username);
+		return refKey.equals(userKey);
+	}
+
+	void doGetEntity(String id, HttpServletRequest req, HttpServletResponse resp)
+		throws IOException
+	{
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Key key = KeyFactory.createKey("Tournament", id);
+		Entity ent;
+		try {
+			ent = datastore.get(key);
+		}
+		catch (EntityNotFoundException e) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+
+		resp.setContentType("text/json;charset=UTF-8");
+		JsonGenerator out = new JsonFactory().
+			createJsonGenerator(resp.getWriter()
+			);
+
+		out.writeStartObject();
+		out.writeStringField("id", id);
+		out.writeStringField("title", (String)ent.getProperty("title"));
+		out.writeBooleanField("can_admin", isUser(req, (Key) ent.getProperty("owner")));
+		out.writeEndObject();
+		out.close();
 	}
 
 	void doGetIndex(HttpServletRequest req, HttpServletResponse resp)
